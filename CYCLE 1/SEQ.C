3ec1834aad570c9c5ec23ac9caf90b03e87eed72
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2022-11-28 22:09:09 +0530

    Create SEQ.C

---
+++ CYCLE 1/SEQ.C
@@ -0,0 +1,32 @@
+#include<stdio.h>
+# include<stdlib.h>
+void main()
+{
+ int f[50],i,st,len,j,c,k,count=0;
+for(i=0;i<50;i++)
+f[i]=0;
+printf("files allocated are:\n");
+x:count=0;
+printf("enter starting block and length");
+scanf("%d%d",&st,&len);
+for(k=st;k<(st+len);k++)
+if(f[k]==0)
+count++;
+if(len==count)
+{
+for(j=st;j<(st+len);j++)
+if(f[j]==0)
+{
+f[j]=1;
+printf("%d\t%d\n",j,f[j]);
+}
+if(j!=(st+len-1))
+printf("the file is allocated to the disk\n");
+}
+else
+printf("the file is not allocated\n");
+printf("do you want to enter more file(yes-1/no-0)");
+scanf("%d",&c);
+if(c==1)
+goto x;
+}
